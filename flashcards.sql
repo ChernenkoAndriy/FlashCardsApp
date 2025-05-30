ab0c4cd6739eb70727715d44ddfa5abf53cb88d3
Diana Tsisaruk <d.tsisaruk@ukma.edu.ua>
2025-05-31 02:52:30 +0300

    advanced db

--- flashcards.sql
+++ flashcards.sql
@@ -8,6 +8,7 @@ CREATE TABLE IF NOT EXISTS `user`
     `username` varchar(50)  NOT NULL,
     `email`    varchar(100) NOT NULL,
     `password` varchar(255) NOT NULL,
+    `language` varchar(30)  NOT NULL,
     PRIMARY KEY (`id`),
     UNIQUE KEY `username` (`username`)
 ) ENGINE = InnoDB
@@ -15,27 +16,38 @@ CREATE TABLE IF NOT EXISTS `user`
   AUTO_INCREMENT = 1;
 
 
-INSERT INTO `user` (`username`, `email`, `password`)
-VALUES ('admin', 'admin@gmail.com', 'admin777');
+INSERT INTO `user` (`username`, `email`, `password`, `language`)
+VALUES ('admin', 'admin@gmail.com', 'admin777', 'Ukrainian');
 
+CREATE TABLE IF NOT EXISTS `language`
+(
+    `id`   int(11)     NOT NULL AUTO_INCREMENT,
+    `name` varchar(50) NOT NULL,
+    PRIMARY KEY (`id`),
+    UNIQUE KEY `name` (`name`)
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  AUTO_INCREMENT = 1;
+
+INSERT INTO `language` (`name`)
+VALUES ('English');
 
 CREATE TABLE IF NOT EXISTS `deck`
 (
-    `id`            int(11)     NOT NULL AUTO_INCREMENT,
-    `name`          varchar(50) NOT NULL,
-    `language_from` varchar(30) NOT NULL,
-    `language_to`   varchar(30) NOT NULL,
-    `workload`      int(11)     NOT NULL DEFAULT 0,
-    `user_id`       int(11)     NOT NULL,
+    `id`           int(11)     NOT NULL AUTO_INCREMENT,
+    `name`         varchar(50) NOT NULL,
+    `cards_number` int(11)     NOT NULL DEFAULT 0,
+    `is_active`    bool        NOT NULL DEFAULT 1,
+    `language_id`  int(11)     NOT NULL,
     PRIMARY KEY (`id`),
-    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+    FOREIGN KEY (`language_id`) REFERENCES `language` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
 ) ENGINE = InnoDB
   DEFAULT CHARSET = utf8mb4
   AUTO_INCREMENT = 1;
 
 
-INSERT INTO `deck` (`name`, `language_from`, `language_to`, `workload`, `user_id`)
-VALUES ('TEST', 'Ukrainian', 'English', 10, 1);
+INSERT INTO `deck` (`name`, `language_id`, `is_active`)
+VALUES ('TEST', 1, 1);
 
 CREATE TABLE IF NOT EXISTS `card`
 (
@@ -53,5 +65,35 @@ CREATE TABLE IF NOT EXISTS `card`
   AUTO_INCREMENT = 1;
 
 INSERT INTO `card` (`word`, `translate`, `definition`, `image`, `is_learned`, `deck_id`)
-VALUES ('test', 'тест', 'a procedure intended to establish the quality, performance, or reliability of something', NULL, 0, 1);
+VALUES ('test', 'тест', 'a procedure intended to establish the quality, performance, or reliability of something', NULL,
+        0, 1);
 
+
+CREATE TABLE IF NOT EXISTS `user_progress`
+(
+    `id`         int(11)                                       NOT NULL AUTO_INCREMENT,
+    `user_id`    int(11)                                       NOT NULL,
+    `card_id`    int(11)                                       NOT NULL,
+    `period`     enum ('learning', 'first', 'second', 'third') NOT NULL DEFAULT 'new',
+    `is_correct` bool                                          NOT NULL DEFAULT 0,
+    date         datetime                                      NOT NULL DEFAULT CURRENT_TIMESTAMP,
+    PRIMARY KEY (`id`),
+    FOREIGN KEY (`user_id`) REFERENCES `user` (`id`) ON DELETE CASCADE ON UPDATE CASCADE,
+    FOREIGN KEY (`card_id`) REFERENCES `card` (`id`) ON DELETE CASCADE ON UPDATE CASCADE
+) ENGINE = InnoDB
+  DEFAULT CHARSET = utf8mb4
+  AUTO_INCREMENT = 1;
+
+INSERT INTO `user_progress` (`user_id`, `card_id`, `period`, `is_correct`)
+VALUES (1, 1, 'learning', 0);
+
+
+CREATE TRIGGER `update_deck_cards_number`
+    AFTER INSERT
+    ON `card`
+    FOR EACH ROW
+BEGIN
+    UPDATE `deck`
+    SET `cards_number` = `cards_number` + 1
+    WHERE `id` = NEW.`deck_id`;
+END;
