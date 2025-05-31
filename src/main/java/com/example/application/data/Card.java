a1cef793c4f333ef88af200a93328c1054ee77b0
Diana Tsisaruk <d.tsisaruk@ukma.edu.ua>
2025-05-31 13:19:23 +0300

    entity-repository-service for card

---
+++ src/main/java/com/example/application/data/Card.java
@@ -0,0 +1,100 @@
+package com.example.application.data;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "`card`")
+public class Card {
+
+    @Id
+    @GeneratedValue
+    @Column(name = "`id`", nullable = false)
+    private Long id;
+
+    @Column(name = "`word`", nullable = false)
+    private String word;
+
+    @Column(name = "`translate`", nullable = false)
+    private String translate;
+
+    @Column(name = "`definition`", nullable = false)
+    private String definition;
+
+    @Column(name = "`image`", nullable = true)
+    private String image;
+
+    @Column(name = "`is_learned`", nullable = false)
+    private boolean isLearned;
+
+    @Column(name = "`deck_id`", nullable = false)
+    private Long deckId;
+
+    public Card(String word, String translate, String definition, String image, boolean isLearned, Long deckId) {
+        this.word = word;
+        this.translate = translate;
+        this.definition = definition;
+        this.image = image;
+        this.isLearned = isLearned;
+        this.deckId = deckId;
+    }
+
+    public Card() {
+    }
+
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getWord() {
+        return word;
+    }
+
+    public void setWord(String word) {
+        this.word = word;
+    }
+
+    public String getTranslate() {
+        return translate;
+    }
+
+    public void setTranslate(String translate) {
+        this.translate = translate;
+    }
+
+    public String getDefinition() {
+        return definition;
+    }
+
+    public void setDefinition(String definition) {
+        this.definition = definition;
+    }
+
+    public String getImage() {
+        return image;
+    }
+
+    public void setImage(String image) {
+        this.image = image;
+    }
+
+    public boolean isLearned() {
+        return isLearned;
+    }
+
+    public void setLearned(boolean learned) {
+        isLearned = learned;
+    }
+
+    public Long getDeckId() {
+        return deckId;
+    }
+
+    public void setDeckId(Long deckId) {
+        this.deckId = deckId;
+    }
+}
---
+++ src/main/java/com/example/application/service/CardService.java
@@ -0,0 +1,32 @@
+package com.example.application.service;
+
+import com.example.application.data.Card;
+import com.example.application.repositories.CardRepository;
+import org.springframework.stereotype.Service;
+import java.util.List;
+
+@Service
+public class CardService {
+
+    private final CardRepository cardRepository;
+
+    public CardService(CardRepository cardRepository) {
+        this.cardRepository = cardRepository;
+    }
+
+    public List<Card> findAll() {
+        return cardRepository.findAll();
+    }
+
+    public List<Card> findByDeckId(Long deckId) {
+        return cardRepository.findByDeckId(deckId);
+    }
+
+    public void save(Card card) {
+        cardRepository.save(card);
+    }
+
+    public void delete(Card card) {
+        cardRepository.delete(card);
+    }
+}
